a6c442d4305af86005e59d71bfc050788f7e9274
Pierre Mesure <pierre@mesu.re>
2021-07-05 11:32:31 +0200

    Add some new columns, the total and a reference to flatgithub.com

--- run.py
+++ run.py
@@ -33,6 +33,10 @@ for instance in instances:
         'app_name': result.application_name(),
         'name': result.name(),
         'version': result.version(),
+        'users_count': result.get_stat('users_count'),
+        'processes_count': result.get_stat('processes_count'),
+        'assemblies_count': result.get_stat('assemblies_count'),
+        'comments_count': result.get_stat('comments_count'),
         'users': result.get_metric('users'),
         'participatory_processes':
         result.get_metric('participatory_processes'),
@@ -45,8 +49,53 @@ for instance in instances:
         'endorsements': result.get_metric('endorsements'),
         'survey_answers': result.get_metric('survey_answers'),
         'results': result.get_metric('results'),
-        'debates': result.get_metric('debates'),
+        'debates': result.get_metric('debates')
     }
     organizations.append(organization)
+
+TOTAL = {
+    'url':
+    '',
+    'app_name':
+    'TOTAL',
+    'name':
+    'TOTAL',
+    'version':
+    '',
+    'users_count':
+    sum(o['users_count'] for o in organizations),
+    'processes_count':
+    sum(o['processes_count'] for o in organizations),
+    'assemblies_count':
+    sum(o['assemblies_count'] for o in organizations),
+    'comments_count':
+    sum(o['comments_count'] for o in organizations),
+    'users':
+    sum(o['users'] for o in organizations),
+    'participatory_processes':
+    sum(o['participatory_processes'] for o in organizations),
+    'assemblies':
+    sum(o['assemblies'] for o in organizations),
+    'comments':
+    sum(o['comments'] for o in organizations),
+    'meetings':
+    sum(o['meetings'] for o in organizations),
+    'proposals':
+    sum(o['proposals'] for o in organizations),
+    'accepted_proposals':
+    sum(o['accepted_proposals'] for o in organizations),
+    'votes':
+    sum(o['votes'] for o in organizations),
+    'endorsements':
+    sum(o['endorsements'] for o in organizations),
+    'survey_answers':
+    sum(o['survey_answers'] for o in organizations),
+    'results':
+    sum(o['results'] for o in organizations),
+    'debates':
+    sum(o['debates'] for o in organizations)
+}
+
+organizations.append(TOTAL)
 
 Writer.write_csv(organizations, 'organizations.csv')
